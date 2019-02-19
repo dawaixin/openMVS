94fa912566da25db7de4607cef29ef5309dd78dc
Raphaël Durand-Delacre <rduranddelacre@telys.com>
2019-02-19 17:12:42 +0000

    Fixed ColourRampMesh stub

--- apps/ColourRampMesh/ColourRampMesh.cpp
+++ apps/ColourRampMesh/ColourRampMesh.cpp
@@ -52,119 +52,121 @@ String strInputFileName;
 String strOutputFileName;
 String strMeshFileName;
 String strDenseConfigFileName;
+String strExportType;
+String strConfigFilename;
 float fSampleMesh;
 int nArchiveType;
 int nProcessPriority;
 unsigned nMaxThreads;
 String strConfigFileName;
 boost::program_options::variables_map vm;
+bool bMeshExport;
 } // namespace OPT
 
 // initialize and parse the command line parameters
 bool Initialize(size_t argc, LPCTSTR* argv)
 {
 	// initialize log and console
-		OPEN_LOG();
-		OPEN_LOGCONSOLE();
+	OPEN_LOG();
+	OPEN_LOGCONSOLE();
 
-		// group of options allowed only on command line
-		boost::program_options::options_description generic("Generic options");
-		generic.add_options()
-			("help,h", "produce this help message")
-			("working-folder,w", boost::program_options::value<std::string>(&WORKING_FOLDER), "working directory (default current directory)")
-			("config-file,c", boost::program_options::value<std::string>(&OPT::strConfigFileName)->default_value(APPNAME _T(".cfg")), "file name containing program options")
-			("export-type", boost::program_options::value<std::string>(&OPT::strExportType)->default_value(_T("ply")), "file type used to export the 3D scene (ply or obj)")
-			("archive-type", boost::program_options::value<unsigned>(&OPT::nArchiveType)->default_value(2), "project archive type: 0-text, 1-binary, 2-compressed binary")
-			("process-priority", boost::program_options::value<int>(&OPT::nProcessPriority)->default_value(-1), "process priority (below normal by default)")
-			("max-threads", boost::program_options::value<unsigned>(&OPT::nMaxThreads)->default_value(0), "maximum number of threads (0 for using all available cores)")
-			#if TD_VERBOSE != TD_VERBOSE_OFF
-			("verbosity,v", boost::program_options::value<int>(&g_nVerbosityLevel)->default_value(
-				#if TD_VERBOSE == TD_VERBOSE_DEBUG
-				3
-				#else
-				2
-				#endif
-				), "verbosity level")
+	// group of options allowed only on command line
+	boost::program_options::options_description generic("Generic options");
+	generic.add_options()
+		("help,h", "produce this help message")
+		("working-folder,w", boost::program_options::value<std::string>(&WORKING_FOLDER), "working directory (default current directory)")
+		("config-file,c", boost::program_options::value<std::string>(&OPT::strConfigFileName)->default_value(APPNAME _T(".cfg")), "file name containing program options")
+		("export-type", boost::program_options::value<std::string>(&OPT::strExportType)->default_value(_T("ply")), "file type used to export the 3D scene (ply or obj)")
+		("archive-type", boost::program_options::value<int>(&OPT::nArchiveType)->default_value(2), "project archive type: 0-text, 1-binary, 2-compressed binary")
+    	("process-priority", boost::program_options::value<int>(&OPT::nProcessPriority)->default_value(-1), "process priority (below normal by default)")
+		("max-threads", boost::program_options::value<unsigned>(&OPT::nMaxThreads)->default_value(0), "maximum number of threads (0 for using all available cores)")
+		#if TD_VERBOSE != TD_VERBOSE_OFF
+		("verbosity,v", boost::program_options::value<int>(&g_nVerbosityLevel)->default_value(
+			#if TD_VERBOSE == TD_VERBOSE_DEBUG
+			3
+			#else
+			2
 			#endif
-			;
+			), "verbosity level")
+		#endif
+		;
 
-		// group of options allowed both on command line and in config file
-			boost::program_options::options_description config_main("I/O options");
-			config_main.add_options()
-				("input-file,i", boost::program_options::value<std::string>(&OPT::strInputFileName), "input filename containing camera poses and image list")
-				("output-file,o", boost::program_options::value<std::string>(&OPT::strOutputFileName), "output filename for storing the mesh")
-				;
-			// hidden options, allowed both on command line and
-			// in config file, but will not be shown to the user
-			boost::program_options::options_description hidden("Hidden options");
-			hidden.add_options()
-				("mesh-export", boost::program_options::value<bool>(&OPT::bMeshExport)->default_value(false), "just export the mesh contained in loaded project")
-				;
+	// group of options allowed both on command line and in config file
+	boost::program_options::options_description config_main("I/O options");
+	config_main.add_options()
+		("input-file,i", boost::program_options::value<std::string>(&OPT::strInputFileName), "input filename containing camera poses and image list")
+		("output-file,o", boost::program_options::value<std::string>(&OPT::strOutputFileName), "output filename for storing the mesh")
+		;
+	// hidden options, allowed both on command line and
+	// in config file, but will not be shown to the user
+	boost::program_options::options_description hidden("Hidden options");
+	hidden.add_options()
+		("mesh-export", boost::program_options::value<bool>(&OPT::bMeshExport)->default_value(false), "just export the mesh contained in loaded project")
+		;
 
-			boost::program_options::options_description cmdline_options;
-			cmdline_options.add(generic).add(config_main).add(hidden);
+	boost::program_options::options_description cmdline_options;
+	cmdline_options.add(generic).add(config_main).add(hidden);
 
-			boost::program_options::options_description config_file_options;
-			config_file_options.add(config_main).add(hidden);
+	boost::program_options::options_description config_file_options;
+	config_file_options.add(config_main).add(hidden);
 
-			boost::program_options::positional_options_description p;
-			p.add("input-file", -1);
+	boost::program_options::positional_options_description p;
+	p.add("input-file", -1);
 
-			try {
-				// parse command line options
-				boost::program_options::store(boost::program_options::command_line_parser((int)argc, argv).options(cmdline_options).positional(p).run(), OPT::vm);
-				boost::program_options::notify(OPT::vm);
-				INIT_WORKING_FOLDER;
-				// parse configuration file
-				std::ifstream ifs(MAKE_PATH_SAFE(OPT::strConfigFileName));
-				if (ifs) {
-					boost::program_options::store(parse_config_file(ifs, config_file_options), OPT::vm);
-					boost::program_options::notify(OPT::vm);
-				}
-			}
-			catch (const std::exception& e) {
-				LOG(e.what());
-				return false;
-			}
-
-			// initialize the log file
-			OPEN_LOGFILE(MAKE_PATH(APPNAME _T("-")+Util::getUniqueName(0)+_T(".log")).c_str());
-
-			// print application details: version and command line
-			Util::LogBuild();
-			LOG(_T("Command line:%s"), Util::CommandLineToString(argc, argv).c_str());
-
-			// validate input
-			Util::ensureValidPath(OPT::strInputFileName);
-			Util::ensureUnifySlash(OPT::strInputFileName);
-			if (OPT::vm.count("help") || OPT::strInputFileName.IsEmpty()) {
-				boost::program_options::options_description visible("Available options");
-				visible.add(generic).add(config_main).add(config_clean);
-				GET_LOG() << visible;
-			}
-			if (OPT::strInputFileName.IsEmpty())
-				return false;
-			OPT::strExportType = OPT::strExportType.ToLower() == _T("obj") ? _T(".obj") : _T(".ply");
-
-			// initialize optional options
-			Util::ensureValidPath(OPT::strOutputFileName);
-			Util::ensureUnifySlash(OPT::strOutputFileName);
-			if (OPT::strOutputFileName.IsEmpty())
-				OPT::strOutputFileName = Util::getFileFullName(OPT::strInputFileName) + _T("_mesh.mvs");
-
-			// initialize global options
-			Process::setCurrentProcessPriority((Process::Priority)OPT::nProcessPriority);
-			#ifdef _USE_OPENMP
-			if (OPT::nMaxThreads != 0)
-				omp_set_num_threads(OPT::nMaxThreads);
-			#endif
-
-			#ifdef _USE_BREAKPAD
-			// start memory dumper
-			MiniDumper::Create(APPNAME, WORKING_FOLDER);
-			#endif
-			return true;
+	try {
+		// parse command line options
+		boost::program_options::store(boost::program_options::command_line_parser((int)argc, argv).options(cmdline_options).positional(p).run(), OPT::vm);
+		boost::program_options::notify(OPT::vm);
+		INIT_WORKING_FOLDER;
+		// parse configuration file
+		std::ifstream ifs(MAKE_PATH_SAFE(OPT::strConfigFileName));
+		if (ifs) {
+			boost::program_options::store(parse_config_file(ifs, config_file_options), OPT::vm);
+			boost::program_options::notify(OPT::vm);
 		}
+	}
+	catch (const std::exception& e) {
+		LOG(e.what());
+		return false;
+	}
+
+	// initialize the log file
+	OPEN_LOGFILE(MAKE_PATH(APPNAME _T("-")+Util::getUniqueName(0)+_T(".log")).c_str());
+
+	// print application details: version and command line
+	Util::LogBuild();
+	LOG(_T("Command line:%s"), Util::CommandLineToString(argc, argv).c_str());
+
+	// validate input
+	Util::ensureValidPath(OPT::strInputFileName);
+	Util::ensureUnifySlash(OPT::strInputFileName);
+	if (OPT::vm.count("help") || OPT::strInputFileName.IsEmpty()) {
+		boost::program_options::options_description visible("Available options");
+		visible.add(generic).add(config_main);
+		GET_LOG() << visible;
+	}
+	if (OPT::strInputFileName.IsEmpty())
+		return false;
+	OPT::strExportType = OPT::strExportType.ToLower() == _T("obj") ? _T(".obj") : _T(".ply");
+
+	// initialize optional options
+	Util::ensureValidPath(OPT::strOutputFileName);
+	Util::ensureUnifySlash(OPT::strOutputFileName);
+	if (OPT::strOutputFileName.IsEmpty())
+		OPT::strOutputFileName = Util::getFileFullName(OPT::strInputFileName) + _T("_mesh.mvs");
+
+	// initialize global options
+	Process::setCurrentProcessPriority((Process::Priority)OPT::nProcessPriority);
+	#ifdef _USE_OPENMP
+	if (OPT::nMaxThreads != 0)
+		omp_set_num_threads(OPT::nMaxThreads);
+	#endif
+
+	#ifdef _USE_BREAKPAD
+	// start memory dumper
+	MiniDumper::Create(APPNAME, WORKING_FOLDER);
+	#endif
+	return true;
 }
 
 // finalize application instance
@@ -194,6 +196,8 @@ int main(int argc, LPCTSTR* argv)
 	// load project
 	if (!scene.Load(MAKE_PATH_SAFE(OPT::strInputFileName)))
 		return EXIT_FAILURE;
+
+	
 
 	Finalize();
 	return EXIT_SUCCESS;
